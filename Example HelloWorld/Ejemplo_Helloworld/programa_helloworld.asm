e1b55e36effaaf117689ace2c202bef5fe338cd7
Yc3t <ycetaibi@gmail.com>
2025-01-24 15:54:52 +0100

    fix

--- Example HelloWorld/Ejemplo_Helloworld/programa_helloworld.asm
+++ Example HelloWorld/Ejemplo_Helloworld/programa_helloworld.asm
@@ -1,7 +1,7 @@
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
             ; Program: Hamming Code Test with UART           
             ; Tests the Hamming encoder/decoder functionality
-            ; 115200bps, 8 data bits, no parity, 1 stop bit, no flow control
+            ; 9600bps, 8 data bits, no parity, 1 stop bit, no flow control
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
             
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -16,23 +16,48 @@
             NAMEREG s4, cont1           ; Delay counter 1
             NAMEREG s5, cont2           ; Delay counter 2
             NAMEREG s6, data_reg        ; Data for Hamming encoding
+            NAMEREG s7, test_data       ; Test pattern counter
             
             ADDRESS 00                  ; Program starts at address 00
 
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
             ; Program start
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-start:      CALL recibe                ; Wait for start character
+start:      LOAD test_data, 00         ; Initialize test pattern
+            
+test_loop:  ; Send test pattern marker
+            LOAD txreg, 54              ; Send 'T' character (ASCII 54h = 'T')
+            CALL transmite
+            LOAD txreg, 3A              ; Send ':' character (ASCII 3Ah = ':')
+            CALL transmite
             
             ; Test Hamming encoding
-            LOAD data_reg, 05          ; Load test data (0101)
-            LOAD s0, 80                ; Set encode bit
-            OR data_reg, s0            ; Combine encode bit with data
+            LOAD data_reg, test_data    ; Load test pattern
+            LOAD s0, 80                 ; Set encode bit
+            OR data_reg, s0             ; Combine encode bit with data
             OUTPUT data_reg, hamming_port ; Send to Hamming encoder
-            INPUT txreg, hamming_port  ; Read encoded result
-            CALL transmite             ; Transmit encoded data
+            INPUT txreg, hamming_port   ; Read encoded result
+            CALL transmite              ; Transmit encoded data
             
-            JUMP start                 ; Loop forever
+            ; Send newline
+            LOAD txreg, 0D             ; Carriage return
+            CALL transmite
+            LOAD txreg, 0A             ; Line feed
+            CALL transmite
+            
+            ; Increment test pattern
+            ADD test_data, 01
+            AND test_data, 0F          ; Keep in 4-bit range
+            
+            ; Delay between tests
+            LOAD cont1, FF
+delay1:     LOAD cont2, FF
+delay2:     SUB cont2, 01
+            JUMP NZ, delay2
+            SUB cont1, 01
+            JUMP NZ, delay1
+            
+            JUMP test_loop             ; Continue with next pattern
 
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
             ; Character reception routine
@@ -76,10 +101,10 @@ next_tx_bit:
             RETURN
 
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            ; 1-bit wait routine (115200 bps)
+            ; 1-bit wait routine (9600 bps)
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-wait_1bit:  LOAD cont1, 03
-espera2:    LOAD cont2, 22
+wait_1bit:  LOAD cont1, 0A             ; Adjusted for 9600 baud
+espera2:    LOAD cont2, 80
 espera1:    SUB cont2, 01
             JUMP NZ, espera1
             SUB cont1, 01
@@ -87,10 +112,10 @@ espera1:    SUB cont2, 01
             RETURN
 
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            ; 0.5-bit wait routine (115200 bps)
+            ; 0.5-bit wait routine (9600 bps)
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-wait_05bit: LOAD cont1, 03
-espera4:    LOAD cont2, 10
+wait_05bit: LOAD cont1, 05             ; Adjusted for 9600 baud
+espera4:    LOAD cont2, 80
 espera3:    SUB cont2, 01
             JUMP NZ, espera3
             SUB cont1, 01
