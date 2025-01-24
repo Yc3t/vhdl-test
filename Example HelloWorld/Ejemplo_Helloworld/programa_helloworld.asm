57f56d5377b0c8a72cccc32cdc6d3dacd135fdb6
Yc3t <ycetaibi@gmail.com>
2025-01-24 15:48:13 +0100

    modified asm

--- Example HelloWorld/Ejemplo_Helloworld/programa_helloworld.asm
+++ Example HelloWorld/Ejemplo_Helloworld/programa_helloworld.asm
@@ -1,30 +1,98 @@
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            ; Program: Hamming Code Test           
+            ; Program: Hamming Code Test with UART           
             ; Tests the Hamming encoder/decoder functionality
+            ; 115200bps, 8 data bits, no parity, 1 stop bit, no flow control
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
             
-            CONSTANT hamming_port, FE    ; Port for Hamming module
-            CONSTANT uart_port, FF       ; UART port
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+            ; Constants and variables declaration
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+            CONSTANT rs232, FF           ; UART port
+            CONSTANT hamming_port, FE    ; Hamming module port
             
-start:      ; Test Hamming encoding
-            LOAD s0, 05                  ; Load test data (0101)
-            LOAD s1, 80                  ; Set encode bit
-            OR s0, s1                    ; Combine encode bit with data
-            OUTPUT s0, hamming_port      ; Send to Hamming encoder
-            INPUT s2, hamming_port       ; Read encoded result
+            NAMEREG s1, txreg           ; Transmission buffer
+            NAMEREG s2, rxreg           ; Reception buffer
+            NAMEREG s3, contbit         ; Bit counter
+            NAMEREG s4, cont1           ; Delay counter 1
+            NAMEREG s5, cont2           ; Delay counter 2
+            NAMEREG s6, data_reg        ; Data for Hamming encoding
             
-            ; Send result over UART
-            CALL transmit_byte
-            
-            JUMP start                   ; Loop forever
+            ADDRESS 00                  ; Program starts at address 00
 
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            ; Transmit byte routine
+            ; Program start
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-transmit_byte:
-            OUTPUT s2, uart_port         ; Send byte
+start:      CALL recibe                ; Wait for start character
+            
+            ; Test Hamming encoding
+            LOAD data_reg, 05          ; Load test data (0101)
+            LOAD s0, 80                ; Set encode bit
+            OR data_reg, s0            ; Combine encode bit with data
+            OUTPUT data_reg, hamming_port ; Send to Hamming encoder
+            INPUT txreg, hamming_port  ; Read encoded result
+            CALL transmite             ; Transmit encoded data
+            
+            JUMP start                 ; Loop forever
+
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+            ; Character reception routine
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+recibe:     ; Wait for start bit
+            INPUT rxreg, rs232
+            AND rxreg, 80
+            JUMP NZ, recibe
+            CALL wait_05bit
+            ; Store 8 data bits
+            LOAD contbit, 09
+next_rx_bit:
+            CALL wait_1bit
+            SR0 rxreg
+            INPUT s0, rs232
+            AND s0, 80
+            OR rxreg, s0
+            SUB contbit, 01
+            JUMP NZ, next_rx_bit
             RETURN
 
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            ; End of program
+            ; Character transmission routine
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+transmite:  ; Send start bit
+            LOAD s0, 00
+            OUTPUT s0, rs232
+            CALL wait_1bit
+            ; Send 8 data bits
+            LOAD contbit, 08
+next_tx_bit:
+            OUTPUT txreg, rs232
+            CALL wait_1bit
+            SR0 txreg
+            SUB contbit, 01
+            JUMP NZ, next_tx_bit
+            ; Send stop bit
+            LOAD s0, FF
+            OUTPUT s0, rs232
+            CALL wait_1bit
+            RETURN
+
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+            ; 1-bit wait routine (115200 bps)
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+wait_1bit:  LOAD cont1, 03
+espera2:    LOAD cont2, 22
+espera1:    SUB cont2, 01
+            JUMP NZ, espera1
+            SUB cont1, 01
+            JUMP NZ, espera2
+            RETURN
+
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+            ; 0.5-bit wait routine (115200 bps)
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+wait_05bit: LOAD cont1, 03
+espera4:    LOAD cont2, 10
+espera3:    SUB cont2, 01
+            JUMP NZ, espera3
+            SUB cont1, 01
+            JUMP NZ, espera4
+            RETURN
