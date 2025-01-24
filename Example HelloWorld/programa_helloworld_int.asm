bfe8eca7c006c3272ba0cda094de2d55519d523b
Yc3t <ycetaibi@gmail.com>
2025-01-24 16:48:55 +0100

    f11

--- Example HelloWorld/programa_helloworld_int.asm
+++ Example HelloWorld/programa_helloworld_int.asm
@@ -1,155 +1,172 @@
-            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            ;                 
-            ;Transmision RS-232 por software. 
-            ;115200bps, 8 data bits, no parity, 1 stop bit, no flow control,
-            ;parte1: transmite por el puerte serie el contenido de la memoria RAM (64 bytes, portid [0-63])
-            ;parte2: genera numeros pseudo-aleatorios, bucle contador+interrupcion para transmitir numero.
-            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;declaracion de constantes y variables
-            	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                  
-            	CONSTANT	rs232, FF		; puerto comunicacion serie es el FF
-            						; rx es el bit 0 del puerto FF(entrada)
-							; tx es el bit 7 del puerto FF(salida), esto es porque
-		;el hyperterminal envia primero el LSB, por eso vamos desplazando a la 
-		;izquierda al recibir, y al enviar tambien, con lo que enviamos de nuevo
-		;el LSB primero como corresponde para que lo entienda el hyperterminal
-            	NAMEREG		s1, txreg		;buffer de transmision
-            	NAMEREG		s2, rxreg		;buffer de recepcion
-		NAMEREG		s3, contbit		;contador de los 8 bits de datos
-		NAMEREG		s4, cont1		;contador de retardo1
-		NAMEREG		s5, cont2		;contador de retardo2
-		;
-		ADDRESS		00			;el programa se cargara a partir de la dir 00
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;Inicio del programa
-            	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		DISABLE INTERRUPT
-start:		CALL		recibe
-		;Instrucciones para la parte1
-		LOAD		S7,00
-parte1:		INPUT		txreg,S7
-		ADD		txreg,00
-		JUMP Z		parte2
-		CALL		transmite
-		ADD		S7,01
-		JUMP		parte1
-		;Instrucciones para la parte2
-parte2:		ENABLE INTERRUPT
-bucle1:		LOAD 		S6,09
-bucle2:		SUB		S6,01
-		JUMP NZ		bucle2
-		LOAD		S6,09
-		JUMP		bucle2
-		
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;Rutina de recepcion de caracteres
-            	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-recibe:		;esperamos a que se reciba un bit de inicio
-		INPUT		rxreg, rs232
-		AND		rxreg, 80
-		JUMP		NZ, recibe
-		CALL		wait_05bit
-		;almacenamos los 8 bits de datos
-		LOAD		contbit,09
-next_rx_bit:	CALL		wait_1bit
-		SR0		rxreg
-		INPUT		s0, rs232
-		AND		s0, 80
-		OR		rxreg, s0
-		SUB 		contbit, 01
-		JUMP		NZ, next_rx_bit
-		RETURN
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;Rutina de transmision de caracteres
-            	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-transmite:	;enviamos un bit de inicio
-		LOAD		s0, 00
-		OUTPUT		s0, rs232
-		CALL		wait_1bit
-		;enviamos los 8 bits de datos
-		LOAD 		contbit, 08
-next_tx_bit:	OUTPUT		txreg, rs232
-		CALL		wait_1bit
-		SR0		txreg
-		SUB 		contbit, 01
-		JUMP		NZ, next_tx_bit
-		;enviamos un bit de parada
-		LOAD		s0, FF
-		OUTPUT		s0, rs232
-		CALL		wait_1bit
-		RETURN
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;Rutina espera 1 bit (a 9600bps)
-            	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;clk=50MHz, 9600bps, cont1=0A, cont2=80
-            	;esta rutina ejecuta 1 + (1 + 10*(1 + 128*2 + 2)) + 1 = 2593 instruciones,
-            	;aproximandose al numero teorico de (104,16us/bit)/(0,04 us/instruc) = 2604,166 instr/bit necesarias.
-            	;clk=40MHz, 57600bps, cont1=05, cont2=21
-            	;esta rutina ejecuta 1 + (1 + 5*(1 + 33*2 + 2)) + 1 =  instruciones,
-            	;aproximandose al numero teorico de (17,36us/bit)/(0,05 us/instruc) = 347,2 instr/bit necesarias.
-            	;clk=50MHz, 115200bps, cont1=03, cont2=22
-            	;esta rutina ejecuta 1 + (1 + 3*(1 + 34*2 + 2)) + 1 = 216 instruciones,
-            	;aproximandose al numero teorico de (8,68us/bit)/(0,04 us/instruc) = 217 instr/bit necesarias.
-            	;clk=50MHz, 230400bps, cont1= 03, cont2= 10
-            	;esta rutina ejecuta 1 + (1 + 3*(1 + 16*2 + 2)) + 1 = 108 instruciones,
-            	;aproximandose al numero teorico de (4,34us/bit)/(0,04 us/instruc) = 108,5 instr/bit necesarias.
-            	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;OJO: con el USB2COM no he conseguido pasar de los 230400bps bien. 
-            	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;clk=50MHz, 460800bps, cont1= 03, cont2=06 OJO:Hay que ponerle 1 menos a cont2 y que tome 
-            	;caracteres ascii de 7 bits para que funcione.
-            	;esta rutina ejecuta 1 + (1 + 3*(1 + 7*2 + 2)) + 1 = 54 instruciones,
-            	;aproximandose al numero teorico de (2,17us/bit)/(0,04 us/instruc) = 54,25 instr/bit necesarias.
-            	;clk=50MHz, 921600bps, cont1=01, cont2=0A NO FUNCIONA
-            	;esta rutina ejecuta 1 + (1 + 1*(1 + 10*2 + 2)) + 1 = 26 instruciones,
-            	;aproximandose al numero teorico de (1,085us/bit)/(0,04 us/instruc) = 27,127 instr/bit necesarias.
-wait_1bit:	LOAD 		cont1, 03  
-espera2:	LOAD		cont2, 22
-espera1:	SUB		cont2, 01
-		JUMP		NZ, espera1
-		SUB		cont1, 01
-		JUMP		NZ, espera2
-		RETURN
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;Rutina espera 0,5 bits (bit de inicio, a 9600bps)
-            	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;clk=50MHz, 9600bps, cont1=05, cont2=80
-            	;1 + (1 + 5*(1 + 128*2 + 2)) + 1 = 1298; aprox = 1302
-            	;clk=40MHz, 57600bps, cont1=03, cont2=1B
-            	;1 + (1 + 3*(1 + 27*2 + 2)) + 1 = 1298; aprox = 173.6
-		;clk=50MHz, 115200bps, cont1=03, cont2=10
-            	;1 + (1 + 3*(1 + 16*2 + 2)) + 1 = 108; aprox = 108.5
-            	;clk=50MHz, 230400bps, cont1= 03, cont2= 07
-            	;1 + (1 + 3*(1 + 7*2 + 2)) + 1 = 54; aprox = 54,25
-            	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;OJO: con el USB2COM no he conseguido pasar de los 230400bps bien. 
-            	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            	;clk=50MHz, 460800bps, cont1= 01, cont2= 0A
-            	;1 + (1 + 1*(1 + 10*2 + 2)) + 1 = 26; aprox = 27,125
-            	;clk=50MHz, 921600bps, cont1=01, cont2=04 NO FUNCIONA
-            	;1 + (1 + 1*(1 + 4*2 + 2)) + 1 = 14; aprox = 13,56
-wait_05bit:	LOAD 		cont1, 03 
-espera4:	LOAD		cont2, 10
-espera3:	SUB		cont2, 01
-		JUMP		NZ, espera3
-		SUB		cont1, 01
-		JUMP		NZ, espera4
-		RETURN
-        	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        	; FIN
-        	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        	; RUTINA DE ATENCION A LA INTERRUPCI N
-        	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-interrup:	DISABLE 	INTERRUPT
-		CALL 		recibe
-		LOAD 		txreg,rxreg
-		CALL 		transmite
-		ADD		S6,30
-		LOAD 		txreg,S6
-		CALL 		transmite
-		RETURNI		ENABLE
-		ADDRESS		FF
-		JUMP		interrup
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                 
+; Higher/Lower Guessing Game
+; 115200bps, 8 data bits, no parity, 1 stop bit, no flow control
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Constants and variables declaration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                  
+CONSTANT rs232, FF       ; UART port
+
+NAMEREG s0, temp        ; Temporary register
+NAMEREG s1, txreg       ; Transmission buffer
+NAMEREG s2, rxreg       ; Reception buffer
+NAMEREG s3, contbit     ; Bit counter
+NAMEREG s4, cont1       ; Delay counter 1
+NAMEREG s5, cont2       ; Delay counter 2
+NAMEREG s6, current     ; Current number
+NAMEREG s7, next        ; Next number
+
+ADDRESS 00              ; Program starts at address 00
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Program start
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+DISABLE INTERRUPT
+
+start:  LOAD current, 01   ; Initial number
+
+new_round:
+        ; Send current number
+        LOAD txreg, current
+        ADD txreg, 30      ; Convert to ASCII
+        CALL transmite
+        LOAD txreg, 0D     ; CR
+        CALL transmite
+        LOAD txreg, 0A     ; LF
+        CALL transmite
+
+        ; Generate next number
+        LOAD temp, current
+        ADD temp, 43       ; Some "random" operation
+        AND temp, 07       ; Keep it within 0-7
+        ADD temp, 01       ; Make it 1-8
+        LOAD next, temp
+
+        ENABLE INTERRUPT   ; Wait for player input
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Main program loop
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+main_loop:
+        LOAD cont1, FF     ; Some delay
+loop1:  LOAD cont2, FF
+loop2:  SUB cont2, 01
+        JUMP NZ, loop2
+        SUB cont1, 01
+        JUMP NZ, loop1
+        JUMP main_loop     ; Loop forever
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Character reception routine
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+recibe: INPUT rxreg, rs232
+        AND rxreg, 80
+        JUMP NZ, recibe
+        CALL wait_05bit
+        LOAD contbit, 09
+next_rx_bit:
+        CALL wait_1bit
+        SR0 rxreg
+        INPUT temp, rs232
+        AND temp, 80
+        OR rxreg, temp
+        SUB contbit, 01
+        JUMP NZ, next_rx_bit
+        RETURN
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Character transmission routine
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+transmite:
+        LOAD temp, 00
+        OUTPUT temp, rs232
+        CALL wait_1bit
+        LOAD contbit, 08
+next_tx_bit:
+        OUTPUT txreg, rs232
+        CALL wait_1bit
+        SR0 txreg
+        SUB contbit, 01
+        JUMP NZ, next_tx_bit
+        LOAD temp, FF
+        OUTPUT temp, rs232
+        CALL wait_1bit
+        RETURN
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Timing routines
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+wait_1bit:
+        LOAD cont1, 03  
+espera2:
+        LOAD cont2, 22
+espera1:
+        SUB cont2, 01
+        JUMP NZ, espera1
+        SUB cont1, 01
+        JUMP NZ, espera2
+        RETURN
+
+wait_05bit:
+        LOAD cont1, 03 
+espera4:
+        LOAD cont2, 10
+espera3:
+        SUB cont2, 01
+        JUMP NZ, espera3
+        SUB cont1, 01
+        JUMP NZ, espera4
+        RETURN
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Interrupt Service Routine
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ADDRESS FF
+isr:    DISABLE INTERRUPT
+        CALL recibe       ; Get player's guess
+
+        ; Echo received character
+        LOAD txreg, rxreg
+        CALL transmite
+
+        ; Check if guess is correct
+        LOAD temp, rxreg
+        SUB temp, 48      ; Convert from ASCII '0'
+        
+        LOAD s0, current
+        SUB s0, next
+        JUMP Z, tie
+
+        JUMP NC, high_guess
+        ; Low guess
+        SUB temp, 02      ; Check if guess was '2' (lower)
+        JUMP NZ, wrong
+        JUMP win
+
+high_guess:
+        ; High guess
+        SUB temp, 01      ; Check if guess was '1' (higher)
+        JUMP NZ, wrong
+        JUMP win
+
+wrong:  LOAD txreg, 45   ; 'X'
+        CALL transmite
+        JUMP end_isr
+
+win:    LOAD txreg, 43   ; 'O'
+        CALL transmite
+        JUMP end_isr
+
+tie:    LOAD txreg, 3D   ; '='
+        CALL transmite
+        JUMP end_isr
+
+end_isr:
+        LOAD txreg, 0D    ; CR
+        CALL transmite
+        LOAD txreg, 0A    ; LF
+        CALL transmite
+        LOAD current, next ; Update current number
+        JUMP new_round
