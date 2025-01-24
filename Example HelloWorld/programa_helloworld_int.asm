f0856d78d8c1354b6dd4761d83b50d0ace9a3c01
Yc3t <ycetaibi@gmail.com>
2025-01-24 17:26:55 +0100

    f14

--- Example HelloWorld/programa_helloworld_int.asm
+++ Example HelloWorld/programa_helloworld_int.asm
@@ -1,214 +1,141 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                 
-; Higher/Lower Guessing Game
-; 115200bps, 8 data bits, no parity, 1 stop bit, no flow control
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Constants and variables declaration
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                  
-CONSTANT rs232, FF       ; UART port
-
-NAMEREG s0, temp        ; Temporary register
-NAMEREG s1, txreg       ; Transmission buffer
-NAMEREG s2, rxreg       ; Reception buffer
-NAMEREG s3, contbit     ; Bit counter
-NAMEREG s4, cont1       ; Delay counter 1
-NAMEREG s5, cont2       ; Delay counter 2
-NAMEREG s6, current     ; Current number
-NAMEREG s7, next        ; Next number
-
-ADDRESS 00              ; Program starts at address 00
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Program start
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-DISABLE INTERRUPT
-
-start:  LOAD current, 01   ; Initial number
-
-new_round:
-        ; Send "Current: "
-        LOAD txreg, 43     ; 'C'
-        CALL transmite
-        LOAD txreg, 75     ; 'u'
-        CALL transmite
-        LOAD txreg, 72     ; 'r'
-        CALL transmite
-        LOAD txreg, 3A     ; ':'
-        CALL transmite
-        LOAD txreg, 20     ; Space
-        CALL transmite
-
-        ; Send current number
-        LOAD txreg, current
-        ADD txreg, 30      ; Convert to ASCII
-        CALL transmite
-        LOAD txreg, 0D     ; CR
-        CALL transmite
-        LOAD txreg, 0A     ; LF
-        CALL transmite
-
-        ; Generate next number
-        LOAD temp, current
-        ADD temp, 43       ; Some "random" operation
-        AND temp, 07       ; Keep it within 0-7
-        ADD temp, 01       ; Make it 1-8
-        LOAD next, temp
-
-        ; Send prompt
-        LOAD txreg, 31     ; '1'
-        CALL transmite
-        LOAD txreg, 3D     ; '='
-        CALL transmite
-        LOAD txreg, 48     ; 'H'
-        CALL transmite
-        LOAD txreg, 20     ; Space
-        CALL transmite
-        LOAD txreg, 32     ; '2'
-        CALL transmite
-        LOAD txreg, 3D     ; '='
-        CALL transmite
-        LOAD txreg, 4C     ; 'L'
-        CALL transmite
-        LOAD txreg, 3A     ; ':'
-        CALL transmite
-        LOAD txreg, 20     ; Space
-        CALL transmite
-
-        ENABLE INTERRUPT   ; Wait for player input
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Main program loop
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-main_loop:
-        LOAD cont1, FF     ; Some delay
-loop1:  LOAD cont2, FF
-loop2:  SUB cont2, 01
-        JUMP NZ, loop2
-        SUB cont1, 01
-        JUMP NZ, loop1
-        JUMP main_loop     ; Loop forever
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Character reception routine
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-recibe: INPUT rxreg, rs232
-        AND rxreg, 80
-        JUMP NZ, recibe
-        CALL wait_05bit
-        LOAD contbit, 09
-next_rx_bit:
-        CALL wait_1bit
-        SR0 rxreg
-        INPUT temp, rs232
-        AND temp, 80
-        OR rxreg, temp
-        SUB contbit, 01
-        JUMP NZ, next_rx_bit
-        RETURN
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Character transmission routine
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-transmite:
-        LOAD temp, 00
-        OUTPUT temp, rs232
-        CALL wait_1bit
-        LOAD contbit, 08
-next_tx_bit:
-        OUTPUT txreg, rs232
-        CALL wait_1bit
-        SR0 txreg
-        SUB contbit, 01
-        JUMP NZ, next_tx_bit
-        LOAD temp, FF
-        OUTPUT temp, rs232
-        CALL wait_1bit
-        RETURN
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Timing routines
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-wait_1bit:
-        LOAD cont1, 03  
-espera2:
-        LOAD cont2, 22
-espera1:
-        SUB cont2, 01
-        JUMP NZ, espera1
-        SUB cont1, 01
-        JUMP NZ, espera2
-        RETURN
-
-wait_05bit:
-        LOAD cont1, 03 
-espera4:
-        LOAD cont2, 10
-espera3:
-        SUB cont2, 01
-        JUMP NZ, espera3
-        SUB cont1, 01
-        JUMP NZ, espera4
-        RETURN
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Interrupt Service Routine
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-ADDRESS FF
-isr:    DISABLE INTERRUPT
-        CALL recibe       ; Get player's guess
-
-        ; Echo received character
-        LOAD txreg, rxreg
-        CALL transmite
-        LOAD txreg, 20    ; Space
-        CALL transmite
-
-        ; Check if guess is correct
-        LOAD temp, rxreg
-        SUB temp, 48      ; Convert from ASCII '0'
+        ; Constants and Variables
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        CONSTANT    rs232, FF        ; Serial port
+        CONSTANT    max_attempts, 0A  ; Maximum attempts allowed
         
-        LOAD s0, current
-        SUB s0, next
-        JUMP Z, tie
+        NAMEREG     s0, target       ; Target number to guess
+        NAMEREG     s1, guess        ; User's guess
+        NAMEREG     s2, temp         ; Temporary storage
+        NAMEREG     s3, seed         ; Random number seed
+        NAMEREG     s4, counter      ; Counter for attempts
+        
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ; Main Program
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+start:  
+        ; Initialize random seed
+        LOAD        seed, 42         ; Initial seed
+        
+        ; Generate random number between 1-9
+generate:
+        LOAD        temp, seed
+        AND         temp, 0F         ; Get lower 4 bits
+        SR0         seed             ; Shift right
+        JUMP        NZ, no_feedback
+        XOR         seed, A3         ; XOR with feedback polynomial
+no_feedback:
+        ADD         seed, 00
+        JUMP        Z, generate      ; Ensure not zero
+        LOAD        temp, seed       ; Compare seed with 0A using subtraction
+        SUB         temp, 0A
+        JUMP        NC, generate     ; If seed >= 0A, generate new number
+        
+        ; Store generated number as target
+        LOAD        target, seed
+        LOAD        counter, 00      ; Reset attempt counter
+        
+        ; Welcome message
+        LOAD        s0, 57           ; 'W'
+        OUTPUT      s0, rs232
+        LOAD        s0, 45           ; 'E'
+        OUTPUT      s0, rs232
+        LOAD        s0, 4C           ; 'L'
+        OUTPUT      s0, rs232
+        LOAD        s0, 43           ; 'C'
+        OUTPUT      s0, rs232
+        LOAD        s0, 4F           ; 'O'
+        OUTPUT      s0, rs232
+        LOAD        s0, 4D           ; 'M'
+        OUTPUT      s0, rs232
+        LOAD        s0, 45           ; 'E'
+        OUTPUT      s0, rs232
+        LOAD        s0, 21           ; '!'
+        OUTPUT      s0, rs232
+        LOAD        s0, 0D           ; CR
+        OUTPUT      s0, rs232
+        LOAD        s0, 0A           ; LF
+        OUTPUT      s0, rs232
 
-        JUMP NC, high_guess
-        ; Low guess
-        SUB temp, 02      ; Check if guess was '2' (lower)
-        JUMP NZ, wrong
-        JUMP win
+game_loop:
+        ; Check if max attempts reached
+        LOAD        temp, counter
+        SUB         temp, max_attempts
+        JUMP        Z, game_over
+        
+        ; Get user input and convert from ASCII
+        INPUT       guess, rs232     ; Get character
+        SUB         guess, 30        ; Convert from ASCII
+        ADD         counter, 01      ; Increment attempt counter
+        
+        ; Compare guess with target using subtraction
+        LOAD        temp, guess
+        SUB         temp, target
+        JUMP        Z, correct       ; If guess == target
+        JUMP        C, too_low       ; If guess < target
+        JUMP        too_high         ; If guess > target
+        
+too_high:
+        LOAD        s0, 48           ; 'H'
+        OUTPUT      s0, rs232
+        JUMP        game_loop
+        
+too_low:
+        LOAD        s0, 4C           ; 'L'
+        OUTPUT      s0, rs232
+        JUMP        game_loop
+        
+correct:
+        LOAD        s0, 57           ; 'W'
+        OUTPUT      s0, rs232
+        JUMP        play_again
+        
+game_over:
+        LOAD        s0, 58           ; 'X'
+        OUTPUT      s0, rs232
+        
+play_again:
+        ; Ask to play again
+        INPUT       s0, rs232
+        LOAD        temp, s0
+        SUB         temp, 59         ; Compare with hex 59 ('Y')
+        JUMP        Z, start
+        JUMP        end_program
+        
+end_program:
+        JUMP        end_program      ; Infinite loop at end
 
-high_guess:
-        ; High guess
-        SUB temp, 01      ; Check if guess was '1' (higher)
-        JUMP NZ, wrong
-        JUMP win
+; Subroutines for sending messages
+send_welcome:
+        LOAD        s0, 57           ; 'W'
+        CALL        send_char
+        LOAD        s0, 45           ; 'E'
+        CALL        send_char
+        LOAD        s0, 4C           ; 'L'
+        CALL        send_char
+        LOAD        s0, 43           ; 'C'
+        CALL        send_char
+        LOAD        s0, 4F           ; 'O'
+        CALL        send_char
+        LOAD        s0, 4D           ; 'M'
+        CALL        send_char
+        LOAD        s0, 45           ; 'E'
+        CALL        send_char
+        LOAD        s0, 21           ; '!'
+        CALL        send_char
+        LOAD        s0, 0D           ; CR
+        CALL        send_char
+        LOAD        s0, 0A           ; LF
+        CALL        send_char
+        RETURN
 
-wrong:  LOAD txreg, 45   ; 'X'
-        CALL transmite
-        JUMP show_next
+; Add other message subroutines here...
 
-win:    LOAD txreg, 43   ; 'O'
-        CALL transmite
-        JUMP show_next
-
-tie:    LOAD txreg, 3D   ; '='
-        CALL transmite
-
-show_next:
-        LOAD txreg, 20    ; Space
-        CALL transmite
-        LOAD txreg, 28    ; '('
-        CALL transmite
-        LOAD txreg, next  ; Show next number
-        ADD txreg, 30     ; Convert to ASCII
-        CALL transmite
-        LOAD txreg, 29    ; ')'
-        CALL transmite
-        LOAD txreg, 0D    ; CR
-        CALL transmite
-        LOAD txreg, 0A    ; LF
-        CALL transmite
-        LOAD current, next ; Update current number
-        JUMP new_round
+; Basic I/O subroutines
+send_char:
+        OUTPUT      s0, rs232
+        RETURN
+        
+get_input:
+        INPUT       s0, rs232
+        RETURN
