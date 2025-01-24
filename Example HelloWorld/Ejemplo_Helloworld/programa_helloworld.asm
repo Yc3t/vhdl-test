8e2e2775398cc30a6fb2232714c1130c2fec7b78
Yc3t <ycetaibi@gmail.com>
2025-01-24 16:33:55 +0100

    f9

--- Example HelloWorld/Ejemplo_Helloworld/programa_helloworld.asm
+++ Example HelloWorld/Ejemplo_Helloworld/programa_helloworld.asm
@@ -15,6 +15,7 @@
             NAMEREG s4, cont1       ; Delay counter 1
             NAMEREG s5, cont2       ; Delay counter 2
             NAMEREG s6, secret      ; Secret number to guess
+            NAMEREG s7, temp        ; Temporary storage
             
             ADDRESS 00              ; Program starts at address 00
 
@@ -22,9 +23,6 @@
             ; Program start
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
             DISABLE INTERRUPT
-start:      CALL recibe            ; Wait for start character
-
-            ; Initialize game
             LOAD secret, 05         ; Initial secret number
 
             ; Send welcome message
@@ -37,22 +35,23 @@ start:      CALL recibe            ; Wait for start character
             LOAD txreg, 0A          ; LF
             CALL transmite
 
-            ENABLE INTERRUPT
-bucle1:     LOAD s6, 09            ; Main program loop
-bucle2:     SUB s6, 01             ; like in programa_helloworld_int.asm
-            JUMP NZ, bucle2
-            LOAD s6, 09
-            JUMP bucle2
+            ENABLE INTERRUPT        ; Enable interrupts for game input
+
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+            ; Main program loop
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+main_loop:  LOAD temp, 09          ; Delay loop
+delay:      SUB temp, 01
+            JUMP NZ, delay
+            JUMP main_loop         ; Loop forever
 
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
             ; Character reception routine
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-recibe:     ; Wait for start bit
-            INPUT rxreg, rs232
+recibe:     INPUT rxreg, rs232
             AND rxreg, 80
             JUMP NZ, recibe
             CALL wait_05bit
-            ; Store 8 data bits
             LOAD contbit, 09
 next_rx_bit:
             CALL wait_1bit
@@ -67,11 +66,9 @@ next_rx_bit:
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
             ; Character transmission routine
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-transmite:  ; Send start bit
-            LOAD s0, 00
+transmite:  LOAD s0, 00
             OUTPUT s0, rs232
             CALL wait_1bit
-            ; Send 8 data bits
             LOAD contbit, 08
 next_tx_bit:
             OUTPUT txreg, rs232
@@ -79,14 +76,13 @@ next_tx_bit:
             SR0 txreg
             SUB contbit, 01
             JUMP NZ, next_tx_bit
-            ; Send stop bit
             LOAD s0, FF
             OUTPUT s0, rs232
             CALL wait_1bit
             RETURN
 
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            ; 1-bit wait routine (115200 bps)
+            ; Timing routines
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 wait_1bit:  LOAD cont1, 03  
 espera2:    LOAD cont2, 22
@@ -96,9 +92,6 @@ espera1:    SUB cont2, 01
             JUMP NZ, espera2
             RETURN
 
-            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            ; 0.5-bit wait routine (115200 bps)
-            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 wait_05bit: LOAD cont1, 03 
 espera4:    LOAD cont2, 10
 espera3:    SUB cont2, 01
@@ -107,39 +100,43 @@ espera3:    SUB cont2, 01
             JUMP NZ, espera4
             RETURN
 
-            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
             ; Interrupt Service Routine
-            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
             ADDRESS FF
-interrup:   DISABLE INTERRUPT
-            CALL recibe
-            LOAD txreg, rxreg      ; Echo the received character first
+isr:        DISABLE INTERRUPT
+            CALL recibe            ; Get character
+            
+            ; Echo received character
+            LOAD txreg, rxreg
             CALL transmite
             
-            LOAD s0, rxreg         ; Save original input
-            SUB s0, 48             ; Convert ASCII to number
+            ; Process game logic
+            LOAD temp, rxreg       ; Save input
+            SUB temp, 48          ; Convert from ASCII
             
             ; Compare with secret
-            SUB s0, secret
-            JUMP Z, win
-            JUMP C, low
-
-high:       LOAD txreg, 72         ; 'H'
+            LOAD s0, temp         ; Load guess
+            SUB s0, secret        ; Compare with secret
+            JUMP Z, win           ; If zero, correct guess
+            JUMP C, low           ; If carry, too low
+            
+high:       LOAD txreg, 72        ; 'H'
             CALL transmite
             JUMP end_isr
-
-low:        LOAD txreg, 76         ; 'L'
+            
+low:        LOAD txreg, 76        ; 'L'
             CALL transmite
             JUMP end_isr
-
-win:        LOAD txreg, 42         ; '*'
+            
+win:        LOAD txreg, 42        ; '*'
             CALL transmite
-            ADD secret, 01         ; Change secret number
+            ADD secret, 01        ; New secret number
             AND secret, 07
             ADD secret, 01
-
-end_isr:    LOAD txreg, 0D         ; CR
+            
+end_isr:    LOAD txreg, 0D        ; CR
             CALL transmite
-            LOAD txreg, 0A         ; LF
+            LOAD txreg, 0A        ; LF
             CALL transmite
             RETURNI ENABLE
